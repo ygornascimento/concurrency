15eaf5c9bd22b71f918b4b6f06d3c0f50d97cadb
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-07-24 07:54:13 -0300

    Groups and Semaphores

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,39 +1,9 @@
 import UIKit
 
-let serialQueue = DispatchQueue(label: "swiftlee.serial.queue")
-let concurrentQueue = DispatchQueue(label: "swiftlee.serial.cqueue", attributes: .concurrent)
+for i in 1...10 {
+    guard i.isMultiple(of: 3) else {
+        continue
+    }
 
-serialQueue.async {
-    print("Task 1 started")
-    // Do some work..
-    print("Task 1 workinkg")
-
-    print("Task 1 finished")
+    print(i)
 }
-serialQueue.async {
-    print("Task 2 started")
-    // Do some work..
-    print("Task 2 finished")
-}
-/////////////////////////////////////////////////////////
-concurrentQueue.async {
-    print("Task 1 (C) started")
-    // Do some work..
-    print("Task 1 (C) workinkg")
-
-    print("Task 1 (C) finished")
-}
-concurrentQueue.async {
-    print("Task 2 (C) started")
-    // Do some work..
-    print("Task 2 (C) finished")
-}
-
-
-//Inferring QoS
-let label = "swiftlee.serial.queue"
-let queue = DispatchQueue(label: label,
-                            qos: .userInitiated,
-                            attributes: .concurrent)
-
-
